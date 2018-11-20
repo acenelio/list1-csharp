7d5280146c29ba6835e6d44f26418d907a3eb1e5
Nelio Alves <acenelio@gmail.com>
2018-11-20 11:44:30 -0200

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
