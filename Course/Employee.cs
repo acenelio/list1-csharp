20f0dc893e55648dd1f2768887374ebeba99edb1
acenelio <acenelio@gmail.com>
2020-09-11 11:51:12 -0300

    Update Employee.cs

--- Course/Employee.cs
+++ Course/Employee.cs
@@ -5,7 +5,7 @@ namespace Course {
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Salary { get; set; }
+        public double Salary { get; private set; }
 
         public Employee(int id, string name, double salary) {
             Id = id;
